2c97640cb6d2a2872851abdfade9a21f9a076be5
Stephen Thurman <thurman024@gmail.com>
2018-01-16 21:39:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
     if board_check.all? {|space| space == "X"} || board_check.all? {|space| space == "O"}
       return win_combo
     else
-      false
+      return false
     end
   end
 end
