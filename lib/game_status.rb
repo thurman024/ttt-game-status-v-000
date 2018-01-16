71066ab18bb12d9b281d755af0f72006870061c9
Stephen Thurman <thurman024@gmail.com>
2018-01-16 21:16:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
   end
   WIN_COMBINATIONS.each do |win_combo|
     board_check = [board[win_combo[0]], board[win_combo[1]], board[win_combo[2]]]
-    if board_check.all? {|space| space == "X" || space == "O"}
+    if board_check.all? {|space| space == "X"} || board_check.all? {|space| space == "O"}
       return win_combo
     else
       false
